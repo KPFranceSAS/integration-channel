8cf232c66bf95d426d2c7416c34c788bd4a685ed
Stephane Lanjard <stephane.lanjard@kpsport.com>
2022-01-25 09:25:55 +0100

    =Add resend invoice

---
+++ src/Command/ChannelAdvisor/OrderSendInvoicesErrorCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Command\ChannelAdvisor;
+
+
+use App\Service\ChannelAdvisor\SendInvoicesToChannelAdvisor;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class OrderSendInvoicesErrorCommand extends Command
+{
+    protected static $defaultName = 'app:channel-send-invoices-error';
+    protected static $defaultDescription = 'Send all orders in error transformed in invoices in the ERP';
+
+    public function __construct(SendInvoicesToChannelAdvisor $integrate)
+    {
+        $this->integrate = $integrate;
+        parent::__construct();
+    }
+
+    /**
+     * 
+     *
+     * @var SendInvoicesToChannelAdvisor
+     */
+    private $integrate;
+
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription(self::$defaultDescription);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $this->integrate->processOrders(true);
+
+        return Command::SUCCESS;
+    }
+}
