301133d368155637cfc7cc34d6b4a46e8144d207
Stephane Lanjard <stephane.lanjard@kpsport.com>
2022-03-24 15:12:09 +0100

    =Ajuste purchase dates channel

--- src/Command/Utils/AddPurchaseDateCommand.php
+++ src/Command/Utils/AddPurchaseDateCommand.php
@@ -4,37 +4,49 @@ namespace App\Command\Utils;
 
 use App\Entity\WebOrder;
 use Doctrine\Persistence\ManagerRegistry;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AddPurchaseDateCommand extends Command
 {
-    protected static $defaultName = 'app:change-purchase-dates-aliexpress';
+    protected static $defaultName = 'app:channel-add-purchase-dates';
     protected static $defaultDescription = 'Add purchase dates';
 
-    protected $manager;
-
-    protected $logger;
-
-    public function __construct(LoggerInterface $logger, ManagerRegistry $manager)
+    public function __construct(ManagerRegistry $manager)
     {
-        $this->logger = $logger;
         $this->manager = $manager->getManager();
         parent::__construct();
     }
 
+
+    private $manager;
+
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription(self::$defaultDescription);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-
-        $amzs = $this->manager->getRepository(WebOrder::class)->findBy(['channel' => 'ALIEXPRESS']);
-        foreach ($amzs as $amz) {
-            $purchaseDate = clone ($amz->getPurchaseDate());
-            $purchaseDate->add(new \DateInterval('PT8H'));
-            $amz->setPurchaseDate($purchaseDate);
+        $batchSize = 200;
+        $i = 1;
+        $q = $this->manager->createQuery('select a from App\Entity\WebOrder a where a.channel =:channel')->setParameter('channel', WebOrder::CHANNEL_CHANNELADVISOR);
+        foreach ($q->toIterable() as $webOrder) {
+            $orderApi = $webOrder->getOrderContent();
+            $webOrder->setPurchaseDateFromString($orderApi->CreatedDateUtc);
+            ++$i;
+            if (($i % $batchSize) === 0) {
+                $output->writeln("Saved  $i orders ");
+                $this->manager->flush();
+                $this->manager->clear();
+            }
         }
+        $output->writeln("Test  $i orders ");
         $this->manager->flush();
+
 
         return Command::SUCCESS;
     }
