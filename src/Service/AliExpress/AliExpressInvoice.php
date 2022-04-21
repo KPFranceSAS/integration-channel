5a43c332772c21d9522e10d1c756dc79bc44058b
Stephane Lanjard <stephane.lanjard@kpsport.com>
2022-04-21 09:13:51 +0200

    =Add better control on ALi

--- src/Service/AliExpress/AliExpressInvoice.php
+++ src/Service/AliExpress/AliExpressInvoice.php
@@ -6,12 +6,6 @@ namespace App\Service\AliExpress;
 use App\Entity\WebOrder;
 use App\Helper\Invoice\InvoiceParent;
 use App\Helper\Utils\DatetimeUtils;
-use App\Service\AliExpress\AliExpressApi;
-use App\Service\BusinessCentral\BusinessCentralAggregator;
-use App\Service\Carriers\GetTracking;
-use App\Service\MailService;
-use Doctrine\Persistence\ManagerRegistry;
-use Psr\Log\LoggerInterface;
 
 
 class AliExpressInvoice extends InvoiceParent
@@ -32,12 +26,12 @@ class AliExpressInvoice extends InvoiceParent
         } else {
             $this->addOnlyLogToOrderIfNotExists($order, 'Order was fulfilled by ' . $tracking['Carrier'] . " with tracking number " . $tracking['Tracking number']);
             $order->setTrackingUrl('https://clientesparcel.dhl.es/LiveTracking/ModificarEnvio/' . $tracking['Tracking number']);
-            $result = $this->aliExpressApi->markOrderAsFulfill($order->getExternalNumber(), "SPAIN_LOCAL_DHL", $tracking['Tracking number']);
+            $result = $this->getApi()->markOrderAsFulfill($order->getExternalNumber(), "SPAIN_LOCAL_DHL", $tracking['Tracking number']);
             if ($result) {
                 $this->addLogToOrder($order, 'Mark as fulfilled on Aliexpress');
                 return true;
             } else {
-                $orderAliexpress = $this->aliExpressApi->getOrder($order->getExternalNumber());
+                $orderAliexpress = $this->getApi()->getOrder($order->getExternalNumber());
                 if ($orderAliexpress->logistics_status == "WAIT_SELLER_SEND_GOODS" && $orderAliexpress->order_status == "IN_CANCEL") {
                     $this->addOnlyErrorToOrderIfNotExists($order, 'Error posting tracking number ' . $tracking['Tracking number'] . ' Customer asks for cancelation and no response was done online. A response should be brought before ' . DatetimeUtils::createStringTimeFromAliExpressDate($orderAliexpress->over_time_left));
                 } else if ($orderAliexpress->order_status == 'FINISH' && $orderAliexpress->order_end_reason == "cancel_order_close_trade") {
