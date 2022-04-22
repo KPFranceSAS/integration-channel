c2d5ba057c7385f2829ba3bd96f4eb2b8abe943c
Stephane Lanjard <stephane.lanjard@kpsport.com>
2022-04-22 13:18:13 +0200

    =Actualize financial events

--- src/Command/Amazon/Import/ImportFinancialFilesCommand.php
+++ src/Command/Amazon/Import/ImportFinancialFilesCommand.php
@@ -2,7 +2,13 @@
 
 namespace App\Command\Amazon\Import;
 
+use App\Entity\AmazonFinancialEvent;
 use App\Entity\AmazonFinancialEventGroup;
+use App\Entity\Product;
+use App\Entity\ProductCorrelation;
+use App\Helper\Utils\ExchangeRateCalculator;
+use DateTime;
+use DateTimeZone;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -16,9 +22,10 @@ class ImportFinancialFilesCommand extends Command
     protected static $defaultName = 'app:amz-import-financial-files';
     protected static $defaultDescription = 'Import historical events from Amz';
 
-    public function __construct(ManagerRegistry $manager)
+    public function __construct(ManagerRegistry $manager, ExchangeRateCalculator $exchangeRateCalculator)
     {
         $this->manager = $manager->getManager();
+        $this->exchangeRateCalculator = $exchangeRateCalculator;
         parent::__construct();
     }
 
@@ -29,6 +36,8 @@ class ImportFinancialFilesCommand extends Command
             ->addArgument('pathDirectory', InputArgument::REQUIRED, 'Path of the directories');
     }
 
+
+    private $exchangeRateCalculator;
 
     private $pathDirectory;
 
@@ -57,7 +66,7 @@ class ImportFinancialFilesCommand extends Command
         $files = array_diff(scandir($this->pathDirectory . '/' . $directory), array('..', '.'));
         foreach ($files as $file) {
             $this->output->writeln('---------Start -------' . $file);
-            $this->managerFiles($file);
+            $this->managerFiles($file, $this->pathDirectory . '/' . $directory, $marketplace);
             $this->output->writeln('----------End ----------' . $file);
             $this->output->writeln("");
         }
@@ -68,15 +77,10 @@ class ImportFinancialFilesCommand extends Command
     }
 
 
-    protected function managerFiles($file)
-    {
-        $eventGroupName = str_replace('.txt', '', $file);
-        $this->output->writeln('Import financialEvent  for ' . $eventGroupName);
-        $this->checkAlreadyImported($eventGroupName);
-    }
 
 
-    protected function checkAlreadyImported($eventGroupName)
+
+    protected function cleanBeforeImport($eventGroupName)
     {
         $eventGroup = $this->manager->getRepository(AmazonFinancialEventGroup::class)->findOneBy(["financialEventId" => $eventGroupName]);
         if ($eventGroup) {
@@ -90,19 +94,21 @@ class ImportFinancialFilesCommand extends Command
 
 
 
-    protected function import($eventGroupName)
+    protected function getDataFromFiles($file)
     {
-        $eventGroup = $this->manager->getRepository(AmazonFinancialEventGroup::class)->findOneBy(["financialEventId" => $eventGroupName]);
-        if ($eventGroup) {
-            foreach ($eventGroup->getAmazonFinancialEvents() as $amzFinc) {
-                $this->manager->remove($amzFinc);
-                $eventGroup->removeAmazonFinancialEvent($amzFinc);
+        $contentFiles = file_get_contents($file);
+
+        $datas = [];
+        $contentArray =  explode("\n", $contentFiles);
+        $header = explode("\t", array_shift($contentArray));
+        foreach ($contentArray as $contentLine) {
+            $values = explode("\t", $contentLine);
+            if (count($values) == count($header)) {
+                $datas[] = array_combine($header, $values);
             }
-            $this->manager->flush();
         }
+        return $datas;
     }
-
-
 
 
 
@@ -112,6 +118,116 @@ class ImportFinancialFilesCommand extends Command
             return "Amazon.co.uk";
         } else {
             return 'Amazon.' . strtolower($locale);
+        }
+    }
+
+
+
+    protected function managerFiles($file, $directory, $marketplace)
+    {
+        $eventGroupName = str_replace('.txt', '', $file);
+        $this->output->writeln('Import financialEvent  for ' . $eventGroupName);
+        $this->cleanBeforeImport($eventGroupName);
+        $datas = $this->getDataFromFiles($directory . '/' . $file);
+        $this->output->writeln('Import form files ' . count($datas));
+        $eventGroup = $this->manager->getRepository(AmazonFinancialEventGroup::class)->findOneBy(["financialEventId" => $eventGroupName]);
+        $currency = $this->getCurrency($marketplace);
+        $this->output->writeln('Currency  ' . $currency . ' >>  Marketplace ' . $marketplace);
+
+        unset($datas[0]);
+        //array_pop($datas);
+        //array_pop($datas);
+
+        foreach ($datas as $data) {
+            $amzFinancialEvent = new AmazonFinancialEvent();
+            $amzFinancialEvent->setAdjustmentId($data["adjustment-id"]);
+            $amzFinancialEvent->setAmazonOrderId($data["order-id"]);
+            $amzFinancialEvent->setPostedDate($this->convertDatetime($data["posted-date-time"]));
+            $amount = floatval(str_replace(',', '.', $data["amount"]));
+
+            $amzFinancialEvent->setAmount($this->exchangeRateCalculator->getConvertedAmountDate($amount, $currency, $amzFinancialEvent->getPostedDate()));
+            $amzFinancialEvent->setAmountCurrency($amount);
+            $amzFinancialEvent->setAmountDescription($this->convertAmountDescription($data["amount-description"]));
+            $amzFinancialEvent->setAmountType($data["amount-type"]);
+            $amzFinancialEvent->setMarketplaceName($marketplace);
+            $amzFinancialEvent->setOrderItemCode($this->convertEmptyToNull($data["promotion-id"]));
+
+
+            $amzFinancialEvent->setPromotionId($this->convertEmptyToNull($data["promotion-id"]));
+            $amzFinancialEvent->setQtyPurchased($this->convertEmptyToNull($data["quantity-purchased"]));
+            $amzFinancialEvent->setSellerOrderId($this->convertEmptyToNull($data["merchant-order-id"]));
+            $amzFinancialEvent->setShipmentId($this->convertEmptyToNull($data["shipment-id"]));
+
+            $amzFinancialEvent->setTransactionType($this->convertTransactionType($data["transaction-type"]));
+
+            $sku = $this->convertTransactionType($data["sku"]);
+            if ($sku) {
+                $amzFinancialEvent->setSku($sku);
+                $amzFinancialEvent->setProduct($this->getProductBySku($sku));
+            }
+
+
+            $this->manager->persist($amzFinancialEvent);
+            $eventGroup->addAmazonFinancialEvent($amzFinancialEvent);
+        }
+
+        $this->manager->flush();
+        $this->manager->clear();
+    }
+
+
+    protected function getProductBySku($sku)
+    {
+        $skuSanitized = strtoupper($sku);
+        $productCorrelation = $this->manager->getRepository(ProductCorrelation::class)->findOneBy(['skuUsed' => $skuSanitized]);
+        $sku = $productCorrelation ? $productCorrelation->getSkuErp() : $skuSanitized;
+
+        $product = $this->manager->getRepository(Product::class)->findOneBy([
+            "sku" => $sku
+        ]);
+        return $product;
+    }
+
+
+    protected function convertDatetime($value)
+    {
+        return DateTime::createFromFormat('d.m.Y H:i:s', substr($value, 0, 19));
+    }
+
+    protected function getCurrency($value)
+    {
+        return substr($value, -2) == 'uk' ? 'GBP' : 'EUR';
+    }
+
+
+    protected function convertEmptyToNull($value)
+    {
+        return strlen($value) == 0 ? null : $value;
+    }
+
+    protected function convertAmountDescription($type)
+    {
+        if ($type == "Previous Reserve Amount Balance") {
+            return 'ReserveCredit';
+        } else if ($type == "Current Reserve") {
+            return 'ReserveDebit';
+        } else {
+            return $type;
+        }
+    }
+
+
+
+    protected function convertTransactionType($type)
+    {
+        if ($type == "Order") {
+            return 'ShipmentEvent';
+        } else if ($type == "other-transaction") {
+            return 'AdjustmentEvent';
+        } else if ($type == "Refund") {
+            return 'RefundEvent';
+        } else {
+            return $type;
         }
     }
 }
