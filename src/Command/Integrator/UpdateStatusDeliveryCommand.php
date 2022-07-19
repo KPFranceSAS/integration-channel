45f36e192b4789f91d325bcb0916c974de32d25c
Stephane Lanjard <stephane.lanjard@kpsport.com>
2022-07-19 15:33:17 +0200

    =Add new exports for power bi

--- src/Command/Integrator/UpdateStatusDeliveryCommand.php
+++ src/Command/Integrator/UpdateStatusDeliveryCommand.php
@@ -2,7 +2,7 @@
 
 namespace App\Command\Integrator;
 
-use App\Service\BusinessCentral\UpdateStatusDelivery;
+use App\Service\BusinessCentral\GetStatusDelivery;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,7 +12,7 @@ class UpdateStatusDeliveryCommand extends Command
     protected static $defaultName = 'app:update-status-delivery';
     protected static $defaultDescription = 'Update status from file in the ERP';
 
-    public function __construct(UpdateStatusDelivery $updateStatusDelivery)
+    public function __construct(GetStatusDelivery $updateStatusDelivery)
     {
         $this->updateStatusDelivery = $updateStatusDelivery;
         parent::__construct();
