9eb934847b672678379000a8305fb133775330f3
Stephane Lanjard <stephane.lanjard@kpsport.com>
2022-12-14 17:40:33 +0100

    =Arise on line

---
+++ src/Channels/Arise/Amazfit/AmazfitSyncProduct.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Channels\Arise\Gadget;
+
+use Akeneo\Pim\ApiClient\Search\SearchBuilder;
+use App\Channels\Arise\AriseSyncProductParent;
+use App\Entity\IntegrationChannel;
+
+class SonosSyncProduct extends AriseSyncProductParent
+{
+    protected function getProductsEnabledOnChannel()
+    {
+        $searchBuilder = new SearchBuilder();
+        $searchBuilder
+            ->addFilter('arise_category_id', 'NOT EMPTY')
+            ->addFilter('brand', 'NOT EMPTY')
+            ->addFilter('marketplaces_assignement', 'IN', ['arise_amazfit_es_gi'])
+            ->addFilter('enabled_channel', '=', true, ['scope' => 'Marketplace'])
+            ->addFilter('enabled', '=', true);
+
+        return $this->akeneoConnector->searchProducts($searchBuilder, 'Marketplace');
+    }
+
+    
+
+    public function getChannel(): string
+    {
+        return IntegrationChannel::CHANNEL_SONOS_ARISE;
+    }
+
+    protected function getLocale()
+    {
+        return 'es_ES';
+    }
+}
