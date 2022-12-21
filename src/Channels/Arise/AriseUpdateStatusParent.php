40440ac8769464c0d6dfb14dfdd2eaddc4f5e170
Stephane Lanjard <stephane.lanjard@kpsport.com>
2022-12-21 18:02:26 +0100

    =Add arise change

--- src/Channels/Arise/AriseUpdateStatusParent.php
+++ src/Channels/Arise/AriseUpdateStatusParent.php
@@ -45,4 +45,39 @@ abstract class AriseUpdateStatusParent extends UpdateStatusParent
             return false;
         }
     }
+
+
+
+    protected function updateStatusSaleOrderFulfiledByExternal(WebOrder $order)
+    {
+        $this->logger->info('Update status order prepared by seller and fulfiled by external');
+        $statusSaleOrder = $this->getSaleOrderStatus($order);
+
+        if (in_array($statusSaleOrder['statusCode'], ["99", "-1", "0", "1", "2"])) {
+            $this->addOnlyLogToOrderIfNotExists($order, 'Order status in BC >'.$statusSaleOrder['statusLabel'] .' statusCode '.$statusSaleOrder['statusCode']);
+            if ($statusSaleOrder['statusCode']=="99" || $statusSaleOrder['statusCode']=="-1") {
+                $this->checkShipmentIsLate($order);
+            }
+            $this->checkOrderIsLate($order);
+            return;
+        }
+
+        if (in_array($statusSaleOrder['statusCode'], ["3", "4"]) && strlen($statusSaleOrder['InvoiceNo'])) {
+            $this->addOnlyLogToOrderIfNotExists($order, 'Warehouse shipment created in the ERP with number ' . $statusSaleOrder['ShipmentNo']);
+            $this->addOnlyLogToOrderIfNotExists($order, 'Invoice created in the ERP with number ' . $statusSaleOrder['InvoiceNo']);
+            $businessCentralConnector   = $this->getBusinessCentralConnector($order->getCompany());
+            $invoice =  $businessCentralConnector->getSaleInvoiceByNumber($statusSaleOrder['InvoiceNo']);
+            if ($invoice) {
+                $order->cleanErrors();
+                $postUpdateStatus = $this->postUpdateStatusInvoice($order, $invoice);
+                if ($postUpdateStatus) {
+                    $order->setInvoiceErp($invoice['number']);
+                    $order->setErpDocument(WebOrder::DOCUMENT_INVOICE);
+                    $order->setStatus(WebOrder::STATE_INVOICED);
+                }
+            } else {
+                $this->addOnlyLogToOrderIfNotExists($order, 'Invoice ' . $statusSaleOrder['InvoiceNo']." is not accesible through API");
+            }
+        }
+    }
 }
