efdba675a162534fca17baa974b7012f5f705f1a
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-01-05 16:13:24 +0100

    =Add new dashboard

---
+++ src/Command/Channels/Arise/Sonos/SaveCancelSonosCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Command\Channels\Arise\Sonos;
+
+use App\Command\Channels\Arise\AriseSaveCancelCommand;
+use App\Entity\IntegrationChannel;
+
+class SaveCancelSonosCommand extends AriseSaveCancelCommand
+{
+    protected static $defaultName = 'app:arise-sonos-cancel-orders';
+    protected static $defaultDescription = 'Retrieve all arise sonos orders cancelled online';
+
+
+    protected function getChannel()
+    {
+        return IntegrationChannel::CHANNEL_SONOS_ARISE;
+    }
+}
