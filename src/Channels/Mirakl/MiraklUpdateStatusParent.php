2a43bba83bbba68e58647a9cf26da0edaaa4c90d
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-02-27 17:05:53 +0100

    =Integration decathlon

--- src/Channels/Mirakl/MiraklUpdateStatusParent.php
+++ src/Channels/Mirakl/MiraklUpdateStatusParent.php
@@ -7,7 +7,6 @@ use App\Entity\WebOrder;
 use App\Service\Aggregator\UpdateStatusParent;
 use App\Service\Carriers\DhlGetTracking;
 
-
 abstract class MiraklUpdateStatusParent extends UpdateStatusParent
 {
     protected function getMiraklApi(): MiraklApiParent
@@ -18,44 +17,47 @@ abstract class MiraklUpdateStatusParent extends UpdateStatusParent
 
     protected function postUpdateStatusDelivery(WebOrder $order, $invoice, $trackingNumber=null)
     {
-            $codeCarrier = $this->getCodeCarrier($order->getCarrierService());
-            $nameCarrier = $this->getNameCarrier($order->getCarrierService());
+        $codeCarrier = $this->getCodeCarrier($order->getCarrierService());
+        $nameCarrier = $this->getNameCarrier($order->getCarrierService());
             
-            if(!$codeCarrier || !$nameCarrier){
-                $this->addLogToOrder($order, 'Carrier code is not setup for channel ' . $order->getCarrierService());
-                return false;
-            }
+        if (!$codeCarrier || !$nameCarrier) {
+            $this->addLogToOrder($order, 'Carrier code is not setup for channel ' . $order->getCarrierService());
+            return false;
+        }
 
-            $result = $this->getMiraklApi()->markOrderAsFulfill(
-                $order->getExternalNumber(), 
-                $codeCarrier,
-                $nameCarrier, 
-                $this->trackingAggregator->getTrackingUrlBase($order->getCarrierService(), $trackingNumber),
-                $trackingNumber);
-            if ($result) {
-                $this->addLogToOrder($order, 'Mark as fulfilled on '.$this->getChannel());
+
+        $orderApi=$order->getOrderContent();
+
+
+
+        $result = $this->getMiraklApi()->markOrderAsFulfill(
+            $orderApi['id'],
+            $codeCarrier,
+            $nameCarrier,
+            $this->trackingAggregator->getTrackingUrlBase($order->getCarrierService(), $trackingNumber),
+            $trackingNumber
+        );
+        if ($result) {
+            $this->addLogToOrder($order, 'Mark as fulfilled on '.$this->getChannel());
 
                 
-                $businessCentralConnector   = $this->getBusinessCentralConnector($order->getCompany());
-                $this->addLogToOrder($order, 'Retrieve invoice content ' . $invoice['number']);
-                $contentPdf  = $businessCentralConnector->getContentInvoicePdf($invoice['id']);
-                $this->addLogToOrder($order, 'Retrieved invoice content ' . $invoice['number']);
+            $businessCentralConnector   = $this->getBusinessCentralConnector($order->getCompany());
+            $this->addLogToOrder($order, 'Retrieve invoice content ' . $invoice['number']);
+            $contentPdf  = $businessCentralConnector->getContentInvoicePdf($invoice['id']);
+            $this->addLogToOrder($order, 'Retrieved invoice content ' . $invoice['number']);
                 
 
-               $result = $this->getMiraklApi()->sendInvoice($order->getExternalNumber(), $invoice['number'], $contentPdf);
-               $this->addLogToOrder($order, 'Invoice ' . $invoice['number'].' uploaded on '.$this->getChannel());
+            $result = $this->getMiraklApi()->sendInvoice($orderApi['id'], $invoice['number'], $contentPdf);
+            $this->addLogToOrder($order, 'Invoice ' . $invoice['number'].' uploaded on '.$this->getChannel());
 
-                return true;
-            } else {
-                $this->addLogToOrder($order, 'Error posting tracking number ' . $trackingNumber);
-                return false;
-            }
-       
+            return true;
+        } else {
+            $this->addLogToOrder($order, 'Error posting tracking number ' . $trackingNumber);
+            return false;
+        }
     }
 
     abstract protected function getCodeCarrier(string $codeCarrier): ?string;
 
     abstract protected function getNameCarrier(string $codeCarrier): ?string;
-
-
 }
