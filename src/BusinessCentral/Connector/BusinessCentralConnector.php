597f197c36da979e1f2ed0fbee89fe182c3c3a29
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-03-24 16:39:28 +0100

    =Add restriction length on decatlon

--- src/BusinessCentral/Connector/BusinessCentralConnector.php
+++ src/BusinessCentral/Connector/BusinessCentralConnector.php
@@ -97,7 +97,6 @@ abstract class BusinessCentralConnector
             'base_uri' => $urlBC . '/api/v1.0/',
             'headers'  => [
                 'User-Agent'    => 'Business Central SDK',
-                'Accept-Language' => 'en-US',
                 'Authorization' => "Basic " . base64_encode("$loginBC:$passwordBC"),
             ],
         ]);
