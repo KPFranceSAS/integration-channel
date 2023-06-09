adb73d467b56e184fd074b4fccdfb82f2015da6a
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-06-09 16:03:03 +0200

    =Add new restrictions

--- src/Channels/ChannelAdvisor/ChannelAdvisorUpdateStatus.php
+++ src/Channels/ChannelAdvisor/ChannelAdvisorUpdateStatus.php
@@ -63,16 +63,23 @@ class ChannelAdvisorUpdateStatus extends UpdateStatusParent
             $this->trackingAggregator->getTrackingUrlBase($order->getCarrierService(), $trackingNumber),
             $order->getCarrierService()
         );
-        $businessCentralConnector   = $this->getBusinessCentralConnector($order->getCompany());
-        $this->addLogToOrder($order, 'Retrieve invoice content ' . $invoice['number']);
-        $contentPdf  = $businessCentralConnector->getContentInvoicePdf($invoice['id']);
-        $this->addLogToOrder($order, 'Retrieved invoice content ' . $invoice['number']);
-        $this->addLogToOrder($order, 'Start sending invoice to Channel Advisor');
-        $sendFile = $this->getChannelApi()->sendInvoice($orderApi->ProfileID, $orderApi->ID, $invoice['totalAmountIncludingTax'], $invoice['totalTaxAmount'], $invoice['number'], $contentPdf);
-        if (!$sendFile) {
-            throw new \Exception('Upload  was not done uploaded on ChannelAdvisor for ' . $invoice['number']);
-        }
 
+
+        if(!in_array($order->getSubchannel(), ['CDiscount'])) {
+            $businessCentralConnector   = $this->getBusinessCentralConnector($order->getCompany());
+            $this->addLogToOrder($order, 'Retrieve invoice content ' . $invoice['number']);
+            $contentPdf  = $businessCentralConnector->getContentInvoicePdf($invoice['id']);
+            $this->addLogToOrder($order, 'Retrieved invoice content ' . $invoice['number']);
+            $this->addLogToOrder($order, 'Start sending invoice to Channel Advisor');
+    
+    
+            $sendFile = $this->getChannelApi()->sendInvoice($orderApi->ProfileID, $orderApi->ID, $invoice['totalAmountIncludingTax'], $invoice['totalTaxAmount'], $invoice['number'], $contentPdf);
+            if (!$sendFile) {
+                throw new \Exception('Upload  was not done uploaded on ChannelAdvisor for ' . $invoice['number']);
+            }
+    
+        }
+        
         return true;
 
     }
