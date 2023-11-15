ca32ad338dfd3840de8f71a831657282bc32c185
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-11-15 14:41:39 +0100

    =Change unit discount MM

--- src/Channels/Mirakl/MediaMarkt/MediaMarktPriceStock.php
+++ src/Channels/Mirakl/MediaMarkt/MediaMarktPriceStock.php
@@ -40,7 +40,10 @@ class MediaMarktPriceStock extends MiraklPriceStockParent
                 $offer['price'] = $productMarketplace->getPrice();
                 $promotion = $productMarketplace->getBestPromotionForNow();
                 if($promotion) {
-                    $offer['discount/discount_price'] = $promotion->getPromotionPrice();
+                    $offer['discount'] = [
+                        ['price'=>$promotion->getPromotionPrice()]
+                    ] ;
+                    
                 }
                 
             }
