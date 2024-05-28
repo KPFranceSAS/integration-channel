dd5e58f6f40952e16da77233f5b67268feb6a8da
Stephane Lanjard <stephane.lanjard@kpsport.com>
2024-05-28 14:55:33 +0200

    Move fnac darty

--- src/Channels/FnacDarty/FnacDartySyncProduct.php
+++ src/Channels/FnacDarty/FnacDartySyncProduct.php
@@ -56,69 +56,7 @@ abstract class FnacDartySyncProduct extends MiraklSyncProductParent
             $flatProduct["PCM_Plus_Produit_".$i] = $this->getAttributeSimple($product, 'bullet_point_'.$i, $this->getLocalePim());
         }
 
-
-
-
-        $equivalences = [
-           
-            "marketplace_travel_oven"=>	"1700400030",
-            "marketplace_pizza_peel"	=>"1100500002",
-            "marketplace_pizza_cutter"=>	"1100500002",
-            "marketplace_pizza_brush"	=>"1100500002",
-            "marketplace_pizza_scale"=>	"1100500002",
-            "marketplace_pizza_stone"	=>"1100500002",
-            "marketplace_pizza_roller"=>	"1100500002",
-            "marketplace_pizza_apparel"=>"1100500002",
-            "marketplace_pizza_cooker"	=>"1100500002",
-            "marketplace_pizza_table"	=>"2000200113",
-            "marketplace_pizza_other"=>	"2000200117",
-            "marketplace_video_projectors_video"=>	"401400009",
-            "marketplace_projector_screen"	=>"401400008",
-            "marketplace_projector_stand"	=>"401400002",
-            "marketplace_projector_adapters"=>	"401400002",
-            "marketplace_garden_spa_home"	=>"2000600112",
-            "marketplace_solar_panel_energy_travel"	=>"800400201",
-            "marketplace_generator_energy_travel"	=>"800400324",
-            "marketplace_smart_lock"	=> "1000300012",
-            "marketplace_smart_lock_accesories"=>	"1000300012",
-            'marketplace_solar_panel_mobile' =>	"800400201",
-            'marketplace_camera_waterproof_accessories'  => "400300015",
-            'marketplace_camera_battery' => "400300015",
-            'marketplace_camera_selfie'  => "400300015",
-            'marketplace_camera_charger' => "400300015",
-            'marketplace_camera_stands'  => "400300001",
-            'marketplace_camera_light'   => "400300015",
-            'marketplace_camera_tripod'  => "400300001",
-            'marketplace_camera_accessories' => "400300015",
-            'marketplace_camera_video' =>   "400800004",
-            'marketplace_hair_care_health_personal_care' =>	"1700200006",
-            'marketplace_turntable' =>		'402300004'	,
-            'marketplace_video_3D'	 =>	'401300009'	 ,
-            'marketplace_accessories_other'	 =>	 '401400002',
-            "marketplace_composter_home"  => '2000500301',
-            'marketplace_garden_spa_home_lawn_mowers' => "2000500520",
-            "marketplace_turntable_accessories" => "402400012"
-        ];
-
-
-
-        
-
-        foreach($equivalences as $pimCategory => $mmCategory) {
-            if(in_array($pimCategory, $product['categories'])) {
-                $flatProduct['Typology'] = $mmCategory;
-                break;
-            }
-        }
-
-       
-
-
-        if(array_key_exists('Typology', $flatProduct)) {
-            
-        } else {
-            $this->logger->info('Product not categorized');
-        }
+        $flatProduct['Typology'] = $this->getCategoryNode($this->getAttributeSimple($product, 'mkp_product_type'), 'fnacDarty');
 
         return $flatProduct;
     }
