75032176fcf2a0b1b4f32130756473c4393026ec
Stephane Lanjard <stephane.lanjard@kpsport.com>
2025-01-29 11:56:04 +0100

    Change tracking

--- src/Service/Carriers/SeurTracking.php
+++ src/Service/Carriers/SeurTracking.php
@@ -6,6 +6,6 @@ class SeurTracking
 {
     public static function getTrackingUrlBase($codeTracking)
     {
-        return "http://www.seur.com/livetracking/pages/seguimiento-online-busqueda.do?faces-redirect=true&includeViewParams=true&&segOnlineIdentificador=".$codeTracking;
+        return "https://www.seur.com/miseur/mis-envios?tracking=".$codeTracking;
     }
 }
